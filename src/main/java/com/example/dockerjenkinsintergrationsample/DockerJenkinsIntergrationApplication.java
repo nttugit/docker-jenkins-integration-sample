3cad3c21babb6a01761b1d8ef4f8f5a6603a0a5f
nttu <nttugpt@gmail.com>
2023-08-14 17:24:10 +0700

    First commit

---
+++ src/main/java/com/example/dockerjenkinsintergrationsample/DockerJenkinsIntergrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkinsintergrationsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntergrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntergrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dockerjenkinsintergrationsample/DockerJenkinsIntergrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkinsintergrationsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntergrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
